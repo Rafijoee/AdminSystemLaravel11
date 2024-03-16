cb0f1ac2d3950126b1cbe5a602a094fd42901046
Cikotgr <cikotegar.s@gmail.com>
2024-03-16 22:25:30 +0700

    [fix] project

--- app/Http/Controllers/Users/ProfilesController.php
+++ app/Http/Controllers/Users/ProfilesController.php
@@ -3,13 +3,29 @@
 namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
+use App\Models\Categories;
+use App\Models\Teams;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfilesController extends Controller
 {
     public function index()
     {
+        $user_id = Auth::id();
+        $teams = Teams::where('user_id', $user_id)->first();
+        return view('users.profile.index', compact('teams'));
+    }
+
+    public function store(Request $request)
+    {
         $profile = auth()->user()->profile;
-        return view('users.profile.index', compact('profile'));
+        $profile->update($request->all());
+        return redirect()->back();
+    }
+
+    public function form(){
+        $categories = Categories::all();
+        return view('users.profile.form',compact('categories'));
     }
 }
