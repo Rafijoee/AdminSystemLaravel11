0af2422ddaab13888566f6e4b0bac9975b770b58
Rafijoee <rafijoe44@gmail.com>
2024-05-09 20:51:36 +0700

    fix submission 1

--- app/Http/Controllers/Users/Submissions1Controller.php
+++ app/Http/Controllers/Users/Submissions1Controller.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
+use App\Models\TeamSubmissionsDetails;
 use App\Models\User;
 use Illuminate\Http\Request as HttpRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
 
 class Submissions1Controller extends Controller
 {
@@ -19,7 +22,35 @@ class Submissions1Controller extends Controller
         return view('Users\submisson1\index', compact('categorys_id'));
     }
 
-    public function store (Request $request){
+    public function store(Request $request)
+    {
+        $directory = public_path('submission1');
+        if (!File::isDirectory($directory)) {
+            File::makeDirectory($directory, 0777, true, true);
+        }
+        try {
+            // Validasi request
+            $validator = $request->validate([
+                'submission1' => 'required|mimes:pdf|max:5120',
+            ]);
 
+            if ($request->hasFile('submission1')) {
+                $file = $request->file('submission1');
+                $fileName = time() . '_' . $file->getClientOriginalName();
+                $file->move(public_path('submission1'), $fileName);
+
+                // Simpan data submission ke database
+                $submission = new TeamSubmissionsDetails();
+                $submission->path = 'submission1/' . $fileName;
+                $submission->save();
+
+                return response()->json(['message' => 'File berhasil diupload'], 200);
+            } else {
+                return response()->json(['error' => 'File tidak ditemukan'], 400);
+            }
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->back()->with('error', 'Gagal mengupload proposal');
+        }
     }
 }
--- resources/views/Users/submisson1/index.blade.php
+++ resources/views/Users/submisson1/index.blade.php
@@ -6,7 +6,7 @@
 
                 <div class="mb-6">
                     <label class="block mb-2 text-sm font-medium text-gray-900 " for="file_input">Upload surat keterangan aktif</label>
-                    <input name="" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50  focus:outline-none   " aria-describedby="file_input_help" id="file_input" type="file">
+                    <input name="submission1" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50  focus:outline-none   " aria-describedby="file_input_help" id="file_input" type="file">
                     <p class="mt-1 text-sm text-gray-500 " id="file_input_help">PDF (MAX:5mb)</p>
                 </div>
         </div>
