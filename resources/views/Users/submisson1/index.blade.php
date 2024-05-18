f98e499b4cdaef9ddb1443ed775dd210c82c1303
Rafijoee <rafijoe44@gmail.com>
2024-05-18 09:52:30 +0700

    fix index submission

--- resources/views/Users/submisson1/index.blade.php
+++ resources/views/Users/submisson1/index.blade.php
@@ -1,19 +1,28 @@
 <x-dashboard.layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+    <div class=" bg-white rounded-2xl border-2 mx-20 my-10">
+
+        <div class="mt-10 px-20 ">
+            <h1 class="text-4xl font-bold text-blue-700 my-4">
+                1<sup>st</sup> Stage Proposal Submission </h1>
+            <hr class="border-1 border-gray-400">
             <form action="/submissions1/store" method="POST" enctype="multipart/form-data">
                 @csrf
-
-                <div class="mb-6">
-                    <label class="block mb-2 text-sm font-medium text-gray-900 " for="file_input">Unggah berkas stage 1 dalam bentuk ZIP</label>
-                    <input name="submission1" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50  focus:outline-none   " aria-describedby="file_input_help" id="file_input" type="file">
-                    <p class="mt-1 text-sm text-gray-500 " id="file_input_help">ZIP (MAX:5mb)</p>
+                <h2 class="text-2xl font-semibold my-6">Submit your Documents</h2>
+                <div class="flex items-center justify-center w-full">
+                    <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                        <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                            <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                            </svg>
+                            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                            <p class="text-xs text-gray-500 dark:text-gray-400">ZIP (MAX:5mb)</p>
+                        </div>
+                        <input id="dropzone-file" type="file" class="hidden" name="submission1" />
+                    </label>
                 </div>
+                <button type="submit" class="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 my-10 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit</button>
+            </form>
         </div>
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">kUMPULKAN</button>
     </div>
 
-    </form>
-    </div>
-    </div>
     </x-app-layout>
