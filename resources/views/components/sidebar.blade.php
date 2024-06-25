8cdc172f952c326150d8dfb189cd131e84c0b8e0
Rafijoee <rafijoe44@gmail.com>
2024-06-25 13:20:34 +0700

    fix view

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -43,22 +43,22 @@
                     </a>
                 </li>
                 @auth
-                    @if (optional(auth()->user()->teams)->verified_status != 'unverified' && auth()->user()->teams != null)
-                    <li>
-                        <a href="/submission" class="{{ Request::is('submission') || Request::is('submissions1') || Request::is('submissions2') || Request::is('final-submission') ? 'active' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[#B1E1F0] dark:hover:bg-gray-700 group">
-                            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
-                            </svg>
-                            <span class="flex-1 ms-3 whitespace-nowrap">Submission</span>
-                        </a>
-                    </li>
-                    @endif
+                @if (optional(auth()->user()->teams)->verified_status != 'unverified' && auth()->user()->teams != null)
+                <li>
+                    <a href="/submission" class="{{ Request::is('submission') || Request::is('submissions1') || Request::is('submissions2') || Request::is('final-submission') ? 'active' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[#B1E1F0] dark:hover:bg-gray-700 group">
+                        <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
+                        </svg>
+                        <span class="flex-1 ms-3 whitespace-nowrap">Submission</span>
+                    </a>
+                </li>
+                @endif
                 @endauth
                 @if (auth()->user()?->teams?->stage_id !== 1 && auth()?->user()?->teams?->stage_id !== null && auth()?->user()?->teams?->stage_id !== 4 && auth()?->user()?->teams?->stage_id !== 7 && auth()?->user()?->teams?->stage_id !== 11 && auth()?->user()?->teams?->stage_id !== null)
                 <li>
                     <a href="{{route('payment.index')}}" class="{{ Request::is('payment') ? 'active' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[#B1E1F0] dark:hover:bg-gray-700 group">
                         <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
+                            <path d="M2 4c0-1.1.9-2 2-2h12c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V4zm2 0v12h12V4H4zm2 3h8v1H6V7zm0 3h8v1H6v-1zm0 3h8v1H6v-1z" />
                         </svg>
                         <span class="flex-1 ms-3 whitespace-nowrap">Payment</span>
                     </a>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -142,7 +142,7 @@
                 Join us in exploring new frontiers and pushing boundaries in the digital world.
             </p>
             <div class="mt-4 space-x-4 flex justify-center">
-                <a href="/regiseter" class="text-white bg-[#61C6ED] hover:bg-[#2BBCEE] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Register</a>
+                <a href="/register" class="text-white bg-[#61C6ED] hover:bg-[#2BBCEE] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Register</a>
                 <a href="/login" class="text-white bg-[#61C6ED] hover:bg-[#2BBCEE] focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2">Login</a>
             </div>
         </div>
@@ -203,7 +203,7 @@
                         <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">IT Convert 2024</h5>
                     </a>
                     <p class="mb-3 text-sm font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-whitef rounded-lg bg-[#61C6ED] hover:bg-[#2BBCEE] focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <a href="#" class="inline-flex items-center px-3 py-2 text-white text-sm font-medium text-center text-whitef rounded-lg bg-[#61C6ED] hover:bg-[#2BBCEE] focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Read more
                         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
@@ -233,9 +233,6 @@
     </div>
 
 
-    <div style="margin-top: 50px; display: flex; justify-content: center; align-items: center; height: auto;">
-        <iframe width="1000" height="550" src="https://www.youtube.com/embed/eHPht8UrPWM" frameborder="0" allowfullscreen></iframe>
-    </div>
 
     <div class="flex justify-center items-center my-10" style="margin-top: 50px;">
         <div class="timeline-container w-full max-w-4xl bg-white p-8 rounded-lg shadow-md">
