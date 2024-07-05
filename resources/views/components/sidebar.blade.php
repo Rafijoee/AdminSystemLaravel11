8f46c0b98fb058bd15322dac3f45c1a518f9873a
Rafijoee <rafijoe44@gmail.com>
2024-07-05 16:17:30 +0700

    fix submission

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -5,7 +5,7 @@
         </svg>
     </button>
 
-    <!-- SIDEBAR -->
+    <!-- SIDEBAR Benar -->
     <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 font-sans" aria-label="Sidebar">
         <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
             <a href="/" class="flex items-center ps-2.5 mb-5">
